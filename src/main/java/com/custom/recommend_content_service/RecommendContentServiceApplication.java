fde7823c203d9a569dc1fdc3f0c51a2194d9497e
minim22 <cutesing@naver.com>
2025-11-21 13:31:23 +0900

    feat: 초기 fix

---
+++ src/main/java/com/custom/recommend_content_service/RecommendContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.custom.recommend_content_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendContentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendContentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custom/recommend_content_service/RecommendContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom.recommend_content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
