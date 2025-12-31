4946f64d6405bf67ba57fe2f37825010f4696d03
minim22 <cutesing@naver.com>
2025-12-31 13:08:58 +0900

    feat: 공통부분 수정

--- src/main/java/com/custom/recommend_content_service/exception/ApiException.java
+++ src/main/java/com/custom/recommend_content_service/exception/ApiException.java
@@ -8,9 +8,24 @@ import lombok.Getter;
 public class ApiException extends RuntimeException {
 
     private final ResultCode resultCode;
+    private final String customMessage;
 
+    /**
+     * 기본 응답값
+     * @param resultCode
+     */
     public ApiException(ResultCode resultCode) {
         super(resultCode.getMessage());
         this.resultCode = resultCode;
+        this.customMessage = null;
+    }
+
+    /**
+     * 커스텀 메시지 생성자 - 동적 메시지 사용
+     */
+    public ApiException(ResultCode resultCode, String customMessage) {
+        super(customMessage);
+        this.resultCode = resultCode;
+        this.customMessage = customMessage;
     }
 }
--- src/main/java/com/custom/recommend_content_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/custom/recommend_content_service/exception/GlobalExceptionHandler.java
@@ -1,17 +1,7 @@
 package com.custom.recommend_content_service.exception;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
